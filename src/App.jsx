90c94597cdbf04816f7eec7592937ea068c33820
prakash <p.prakashpaudel35@gmail.com>
2025-02-22 18:16:00 +0530

    Initial commit main branch

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import AddTaskIcon from "@mui/icons-material/AddTask";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-red-500">hiii</p>
+        <AddTaskIcon className="text-green-400" />
+      </div>
+    </>
+  );
+}
+
+export default App;
